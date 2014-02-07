4cf32ba0841f0f9cc88d55c59e1cbd06557995f8
pyromaniac <kinwizard@gmail.com>
2014-02-07 18:35:33 +0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.2'
+gem 'sqlite3'
+
+gem 'chewy', github: 'toptal/chewy'
+gem 'elasticsearch-extensions'
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_girl'
+  gem 'database_cleaner'
+  gem 'ffaker'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChewyExample::Application.config.secret_key_base = '9feff7de4446dc935ef5b1724763346ed7d44c81fbd22722194a967cb50d3eda4307e2f9d2ce84328821baf7eba2988d20f6da27d6acb7acd5d1afb9cca79e8f'
