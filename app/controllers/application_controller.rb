2dd08339d7327c4b8a3fab469592642ee3f5dea4
David Landry <david@dmwl.net>
2014-08-05 21:15:15 -0400

    Update to match docs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  around_action { |&block| Chewy.atomic(&block) }
+  around_action :chewy_block
+  
+  def chewy_atomic
+    Chewy.atomic do
+      yield
+    end
+  end
 end
