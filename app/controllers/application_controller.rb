4d257f1fecdbdc317270eac9aa48775e52e6c6b9
Anton <a.sozontov@gmail.com>
2014-08-06 12:43:20 +0400

    Update application_controller.rb
    fix around_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  around_action :chewy_block
+  around_action :chewy_atomic
   
   def chewy_atomic
     Chewy.atomic do
