e35974b6ae8248c70e8c0b57a586782ea79825c4
pyromaniac <kinwizard@gmail.com>
2014-12-17 23:18:18 +0300

    More chewy update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  around_action :chewy_atomic
-  
-  def chewy_atomic
-    Chewy.atomic do
-      yield
-    end
-  end
 end
--- app/models/dude.rb
+++ app/models/dude.rb
@@ -1,8 +1,8 @@
 class Dude < ActiveRecord::Base
   acts_as_taggable
 
-  has_many :books
-  has_many :videos
+  has_many :books, foreign_key: :author_id
+  has_many :videos, foreign_key: :director_id
   update_index 'entertainment#book', :books
   update_index('entertainment#movie') { videos.movies }
   update_index('entertainment#cartoon') { videos.cartoons }
