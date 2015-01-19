4b579b9e3b5f5ab0932cf75926ba7346bddb1240
pyromaniac <kinwizard@gmail.com>
2015-01-19 07:36:34 +0300

    Wrapping seeds with strategy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-FactoryGirl.create_list :book, 10
-FactoryGirl.create_list :movie, 10
-FactoryGirl.create_list :cartoon, 10
+Chewy.strategy(:atomic) do
+  FactoryGirl.create_list :book, 10
+  FactoryGirl.create_list :movie, 10
+  FactoryGirl.create_list :cartoon, 10
+end
